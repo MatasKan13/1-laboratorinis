f6e9f247ad7fb80470fe40f964bb61001e66e33a
MatasKan13 <kantautasmatas@gmail.com>
2025-09-10 15:27:04 +0300

    Update main.cpp
    Parašyta medianos funkcija ir tikrinamas jos veikimas

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -12,14 +13,28 @@ using std::setw;
 using std::left;
 using std::fixed;
 using std::setprecision;
+using std::sort;
 
 struct Studentas {
     string vardas;
     string pavarde;
     vector <int> paz;
     int egz;
-    double gal;
+    double galVid;
+    double galMed;
 };
+
+double Mediana(vector <int> vekt) {
+    int n = vekt.size();
+    double med;
+    sort(vekt.begin(), vekt.end());
+    if (n % 2 == 0) {
+        med = (vekt[n/2] + vekt[n/2 - 1]) / 2;
+    } else {
+        med = vekt[n/2];
+    }
+    return(med);
+}
 
 Studentas Stud_ivestis(int sk){
     Studentas stud;
@@ -33,20 +48,33 @@ Studentas Stud_ivestis(int sk){
         suma+=laik_paz;
     }
     cout << "Koks egzamino ivertinimas? "; cin >> stud.egz;
-    stud.gal = 0.4 * double(suma)/double(n) + 0.6 * stud.egz;
+    stud.galVid = 0.4 * double(suma)/double(n) + 0.6 * stud.egz;
+    stud.galMed = 0.4 * Mediana(stud.paz) + 0.6 * stud.egz;
     return(stud);
 }
 
 int main() {
-    int m;
+    /*int m;
     vector <Studentas> Grupe;
     cout << "Kiek studentu grupeje? "; cin >> m;
-    for (auto i = 0; i<m; i++) {
+    for (auto i = 0; i < m; i++) {
         Grupe.push_back(Stud_ivestis(i));
     }
     cout << setw(10) << left << "Vardas" << setw(15) << left << "Pavarde" << setw(16) << left << "Galutinis (Vid.)" << endl;
     cout << string(41,'-') << endl;
     for (auto Past : Grupe) {
-        cout << setw(10) << left << Past.vardas << setw(15) << left << Past.pavarde << setw(16) << left << fixed << setprecision(2) << Past.gal << endl;
-    }
+        cout << setw(10) << left << Past.vardas << setw(15) << left << Past.pavarde << setw(16) << left << fixed << setprecision(2) << Past.galVid << endl;
+    }*/
+   vector <int> vekt;
+   for (int i = 24;i>0; i-=3) {
+       i += i%2;
+       vekt.push_back(i);
+   }
+   for (auto i = 0;i < vekt.size(); i++) {
+       cout << vekt[i] << " ";
+   }
+   cout << endl;
+   sort(vekt.begin(), vekt.end());
+   double mediana = Mediana(vekt);
+   cout << mediana;
 }
