dfabad0273cc141e6d164dc9a396a1f97a980746
MatasKan13 <kantautasmatas@gmail.com>
2025-10-14 18:44:51 +0300

    Šablonai, std::list
    Įkeliami sukurti šablonai, pridėtos naujos v0.3 reikalingos bibliotekos

--- mylib.h
+++ mylib.h
@@ -9,6 +9,7 @@
 #include <sstream> // Skirta stringstream (eilutės pavertimo srautu) metodui
 #include <chrono> // Skirta laiko matavimams
 #include <map>
+#include <list>
 
 using std::cout;
 using std::cin;
@@ -33,6 +34,7 @@ using std::stringstream;
 using std::istringstream;
 using std::map;
 using std::to_string;
+using std::list;
 
 struct Studentas {
     string vardas;
@@ -47,11 +49,108 @@ struct Studentas {
 double Mediana(vector <int> vekt);
 Studentas Balo_skaiciavimas(Studentas stud);
 char Iv_raid_patikra(char ivestis, string raides);
-char Iv_paz_patikra(int ivestis);
-Studentas Stud_ivestis(int sk);
 string Failo_patikra(string failo_pav);
-void Failo_nuskaitymas(vector <Studentas> &Grupe, string failo_pav);
-void Paskirstymas(vector <Studentas> &Grupe, vector <Studentas> &Moksliukai, vector <Studentas> &Vargsai);
 char Rikiavimo_tipas();
-void Spausdinimas(vector <Studentas> , char rodinys);
-vector <Studentas> Rikiavimas(vector <Studentas> , char rik);
+
+template <typename T>
+void Failo_nuskaitymas(T &Grupe, string failo_pav) {
+    failo_pav = Failo_patikra(failo_pav);
+    cout << "Puiku! Nuskaitomas failas..." << endl;
+    stringstream buferis;
+    ifstream in(failo_pav);
+    buferis << in.rdbuf();
+    in.close();
+    string stulp;
+    getline(buferis, stulp);
+    while (!buferis.eof()) {
+        Studentas stud;
+        string eil;
+        int pazymys;
+        vector <int> pazymiai;
+        getline(buferis, eil);
+        istringstream srautas(eil);
+        srautas >> stud.vardas >> stud.pavarde;
+        while(srautas >> pazymys) {
+            stud.paz.push_back(pazymys);
+        }
+        stud.egz = stud.paz.back();
+        stud.paz.pop_back();
+        Grupe.push_back(stud);
+    }
+}
+
+template <typename T>
+void Paskirstymas(T &Grupe, T &Moksliukai, T &Vargsai) {
+    for (auto stud : Grupe) {
+        stud = Balo_skaiciavimas(stud);
+        if (stud.islaike) {
+            Moksliukai.push_back(stud);
+        }
+        else {
+            Vargsai.push_back(stud);
+        }
+    }
+    Grupe.clear();
+}
+
+template <typename T>
+void Spausdinimas(T Spausd_gr, char rodinys) {
+    stringstream ss;
+    if (rodinys == 'v') {
+        ss << setw(15) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(16) << left << "Galutinis (Vid.)" << endl;
+        ss << string(51,'-');
+        for (const auto &Past : Spausd_gr) {
+            ss << endl << setw(15) << left << Past.vardas << setw(20) << left << Past.pavarde << setw(16) << left << fixed << setprecision(2) << Past.galVid;
+        }
+    } else if (rodinys == 'm') {
+        ss << setw(15) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(16) << left << "Galutinis (Med.)" << endl;
+        ss << string(51,'-');
+        for (const auto &Past : Spausd_gr) {
+            ss << endl << setw(15) << left << Past.vardas << setw(20) << left << Past.pavarde << setw(16) << left << fixed << setprecision(2) << Past.galMed;
+        }
+    } else if (rodinys == 'a') {
+        ss << setw(15) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(17) << left << "Galutinis (Vid.) " << setw(16) << left << "Galutinis (Med.)" << endl;
+        ss << string(68,'-');
+        for (const auto &Past : Spausd_gr) {
+            ss << endl << setw(15) << left << Past.vardas << setw(20) << left << Past.pavarde << setw(17) << left << fixed << setprecision(2) << Past.galVid << setw(16) << left << fixed << setprecision(2) << Past.galMed;
+        }
+    }
+    try {
+        if (Spausd_gr[0].islaike) {
+            ofstream out("kietiakai.txt");
+            out << ss.str();
+            out.close();
+        } else {
+            ofstream out("nuskriaustukai.txt");
+            out << ss.str();
+            out.close();
+        }
+    }
+    catch(...) {
+        if (Spausd_gr.front().islaike) {
+            ofstream out("kietiakai.txt");
+            out << ss.str();
+            out.close();
+        } else {
+            ofstream out("nuskriaustukai.txt");
+            out << ss.str();
+            out.close();
+        }
+    }
+}
+
+template <typename T>
+T Rikiavimas(T Rikiuojamas, char rik) {
+    sort(Rikiuojamas.begin(), Rikiuojamas.end(), [rik](const Studentas &a, const Studentas &b) {
+        if (rik == 'a') {
+            return a.pavarde < b.pavarde;
+        } else if (rik == 'b') {
+            return a.vardas < b.vardas;
+        } else if (rik == 'c') {
+            return a.galVid > b.galVid;
+        } else {
+            return a.galMed > b.galMed;
+        }
+    });
+    return Rikiuojamas;
+}
